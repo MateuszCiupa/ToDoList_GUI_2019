a3e8b2ddfdb0c84e82f653e59692410b03fc244b
MateuszCiupa <48243124+MateuszCiupa@users.noreply.github.com>
2019-12-08 16:05:50 +0100

    Some commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 const Task = ({ content = "I'm a checkbox!", taskHandle, isChecked }) => (
   <div onClick={taskHandle}>
     <input type="checkbox" defaultChecked={isChecked} />
     <span>{content}</span>
   </div>
-);
+)
 
 const AddTask = ({ addHandle }) => {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState('')
   return (
     <div>
       <h2>Add your task</h2>
@@ -20,15 +20,15 @@ const AddTask = ({ addHandle }) => {
       <button
         type="button"
         onClick={() => {
-          addHandle(value);
-          setValue("");
+          addHandle(value)
+          setValue('')
         }}
       >
         Add
       </button>
     </div>
-  );
-};
+  )
+}
 
 const TaskList = ({ funList, taskHandle }) => (
   <div>
@@ -41,11 +41,11 @@ const TaskList = ({ funList, taskHandle }) => (
       />
     ))}
   </div>
-);
+)
 
 export default () => {
-  const [todoList, setTodoList] = useState([]);
-  const [doneList, setDoneList] = useState([]);
+  const [todoList, setTodoList] = useState([])
+  const [doneList, setDoneList] = useState([])
 
   const addHandle = value => {
     setTodoList(prev =>
@@ -54,22 +54,22 @@ export default () => {
         content: value,
         isChecked: false
       })
-    );
-  };
+    )
+  }
 
   const taskHandle = task => {
     if (task.isChecked) {
-      const clicked = doneList.find(x => x.listId === task.listId);
-      setDoneList(prev => prev.filter(x => x.listId !== task.listId));
-      clicked.isChecked = false;
-      setTodoList(prev => prev.concat(clicked));
+      const clicked = doneList.find(x => x.listId === task.listId)
+      setDoneList(prev => prev.filter(x => x.listId !== task.listId))
+      clicked.isChecked = false
+      setTodoList(prev => prev.concat(clicked))
     } else {
-      const clicked = todoList.find(x => x.listId === task.listId);
-      setTodoList(prev => prev.filter(x => x.listId !== task.listId));
-      clicked.isChecked = true;
-      setDoneList(prev => prev.concat(clicked));
+      const clicked = todoList.find(x => x.listId === task.listId)
+      setTodoList(prev => prev.filter(x => x.listId !== task.listId))
+      clicked.isChecked = true
+      setDoneList(prev => prev.concat(clicked))
     }
-  };
+  }
 
   return (
     <div className="App">
@@ -79,5 +79,5 @@ export default () => {
       <h2>Done</h2>
       <TaskList funList={doneList} taskHandle={taskHandle} />
     </div>
-  );
-};
+  )
+}
